9b172b71e1023c58a0bb831368206a3f659db82a
Nguyen Duc Hai <cockhamphafuqn@gmail.com>
2023-11-07 06:43:39 +0700

    update js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,8 @@
-
-
-var menuLinks = document.querySelectorAll("#nav a");
-menuLinks.forEach(function (link) {
+var menuItems = document.querySelectorAll('#nav a');
+menuItems.forEach(function (link) {
     link.addEventListener("click", function (event) {
         event.preventDefault();
-
         var targetId = this.getAttribute("href");
-
         if (!targetId || targetId.startsWith("http")) {
             window.location.href = targetId;
             return;
@@ -21,7 +17,6 @@ menuLinks.forEach(function (link) {
         }
     });
 });
-var menuItems = document.querySelectorAll('#nav a');
 var header = document.getElementById('header'); // Đảm bảo rằng bạn đã định nghĩa biến "header"
 for (var i = 0; i < menuItems.length; i++) {
     (function (menuItem) {
